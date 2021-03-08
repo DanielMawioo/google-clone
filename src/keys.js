159cdf75d67d157a89c58aed229521ae633be0d5
DanielMawioo <mawioodaniel43@gmail.com>
2021-03-08 10:59:55 +0300

    update commit

--- src/keys.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = " AIzaSyAGdkNBxrf2QyzZJMbBEefF4MDAeAafPA8 ";
-
-export default API_KEY;
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
-import API_KEY from "./keys";
+import API_KEY from "./Keys";
 
-const CONTEXT_KEY = " 9aa3dc53f73aab322";
+const CONTEXT_KEY = "9aa3dc53f73aab322";
 
 const useGoogleSearch = (term) => {
-  const [data, satData] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       fetch(
-        "https://www.googleapis.com/customsearch/v1?key=$ {API_KEY}&cx=${CONTEXT_KEY}&q=${term} "
+        "https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}"
       )
         .then((response) => response.json())
         .then((result) => {
